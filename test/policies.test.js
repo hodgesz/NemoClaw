15e4a394c437e3144f8d88fd1020300cd8e7edab
Tinson Lai <tinsonl@nvidia.com>
2026-04-02 02:23:24 +0000

    fix: add policy preset for brew
    Signed-off-by: Tinson Lai <tinsonl@nvidia.com>

--- test/policies.test.js
+++ test/policies.test.js
@@ -7,9 +7,9 @@ import policies from "../bin/lib/policies";
 
 describe("policies", () => {
   describe("listPresets", () => {
-    it("returns all 9 presets", () => {
+    it("returns all 10 presets", () => {
       const presets = policies.listPresets();
-      expect(presets.length).toBe(9);
+      expect(presets.length).toBe(10);
     });
 
     it("each preset has name and description", () => {
